8b43eee5310e7e786d7e908d5f81b6a92d33688a
rsprasanna <rajupetap@gmail.com>
2020-02-01 23:13:49 -0600

    Added validate password

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ var crypto = require('crypto');
 
     UserSchema.methods.validatePassword =  function(userPassword){
         var hash = crypto.pbkdf2Sync(userPassword, this.salt, 100, 64, 'sha512').toString('hex');
-        return hash = user.passwordHash;
+        return hash = this.passwordHash;
     };
 // Exporting module to allow it to be imported in other files 
 const User = module.exports = mongoose.model('User', UserSchema); 
