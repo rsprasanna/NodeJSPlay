d1407655afd5bb00b10a60e5aa47633a17abcd27
rsprasanna <rajupetap@gmail.com>
2020-02-01 21:06:26 -0600

    Fixed port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const userRouter = require('./routes/users');
 app.use('/api/user', userRouter); 
   
 // Creating server 
-const port = 3000; 
+const port = process.env.PORT || 3000;
 app.listen(port, () => { 
     console.log("Server running at port: " + port); 
 });
